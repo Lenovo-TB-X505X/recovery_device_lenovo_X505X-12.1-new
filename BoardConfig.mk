6525c9980ed31eceb4af65f76a7394c02df77761
Eyad Tamer <eyadtamer2121@gmail.com>
2026-03-15 15:31:02 +0200

    Rearrange flags and add TW_USE_LEGACY_BATTERY_SERVICES
    Added TW_USE_LEGACY_BATTERY_SERVICES because it might fix battery percentage indicator, and rearranged flags for better readability.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,24 +110,33 @@ RECOVERY_TOUCHSCREEN_SWAP_XY := true
 RECOVERY_TOUCHSCREEN_FLIP_Y := true
 
 # TWRP specific build flags
-TW_DEVICE_VERSION := th7
-TW_FRAMERATE := 60
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+TW_FRAMERATE := 60
 TW_EXTRA_LANGUAGES := true
-TW_USE_TOOLBOX := true
-TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_SUPERSU := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TW_HAS_EDL_MODE := true
-TW_INCLUDE_NTFS_3G := true
+
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_OVERRIDE_SYSTEM_PROPS := \ "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 200
+
+TW_USE_TOOLBOX := true
+TW_USE_LEGACY_BATTERY_SERVICES := true
+
+TW_HAS_EDL_MODE := true
+TW_INCLUDE_NTFS_3G := true
+
+TW_OVERRIDE_SYSTEM_PROPS := \ "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_SUPERSU := true
+
 
 # Device Assert
 TARGET_OTA_ASSERT_DEVICE := X505X,TB-X505X,TB-X505F,TB-X505L
