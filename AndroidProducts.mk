9a6bc7a51a96bb972566db99f738f9dcc7362323
ViaanROM <viaan152013@gmail.com>
2026-03-23 07:17:18 +0530

    TB-X505X : Adapt to PBRP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X505X.mk
+    $(LOCAL_DIR)/pb_X505X.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X505X-userdebug \
-    twrp_X505X-user \
-    twrp_X505X-eng
+    pb_X505X-userdebug \
+    pb_X505X-user \
+    pb_X505X-eng
 
